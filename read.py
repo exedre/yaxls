df51d928ed2149713a0dd865c70068fa8340b5d5
Emmanuele Somma <exedre@gmail.com>
2016-09-21 11:08:30 +0200

    read/write cells with xf (simple)

--- read.py
+++ read.py
@@ -1,34 +1,42 @@
 import xlrd
 from collections import defaultdict
 
-def read_xls_into_matrix(fname, sheet_idx):
-    wb = xlrd.open_workbook(fname)
-    sh = wb.sheet_by_index(sheet_idx)
+def read_sheet_into_matrix(sh,xf):
     matrix = []
     for row in range(sh.nrows):
         vcol = []
         for col in range(sh.ncols):
-            vcol.append( sh.cell_value(row,col))
+            cell = sh.cell_value(row,col)
+            if xf:
+                cell = (1, sh.cell(row,col), sh)
+            vcol.append( cell )
         matrix.append(vcol)
     return matrix
-    
 
-def read_xls_into_dictionary(fname, sheet):
-    matrix = read_xls_into_matrix(fname,sheet)
+def read_book_into_matrix(wb, sheet_idx, xf = True):
+    sh = wb.sheet_by_index(sheet_idx)
+    return read_sheet_into_matrix(sh,xf)
+
+def read_xls_into_matrix(fname, sheet_idx, xf = True):
+    wb = xlrd.open_workbook(fname, formatting_info = xf)
+    return read_book_into_matrix(wb,sheet_idx,xf)
+
+
+def read_xls_into_dictionary(fname, sheet, xf = True):
+    matrix = read_xls_into_matrix(fname,sheet, xf)
     headers_def = matrix.pop(0)
     dictionary = defaultdict(list)
     headers = []
     for num,name in enumerate(headers_def):
         if len(name)>0:
             headers.append((num,name))
-    print headers
     for row in matrix:
         for col,name in headers:            
             dictionary[name].append( row[col] )
     return dictionary
 
-def read_xls_into_dataframe(fname, sheet, keyname, multiline=False):
-    matrix = read_xls_into_matrix(fname,sheet)
+def read_xls_into_dataframe(fname, sheet, keyname, multiline=False, xf = True):
+    matrix = read_xls_into_matrix(fname,sheet, xf)
     headers_def = matrix.pop(0)
     dataframe = defaultdict(list)
     headers = []
@@ -48,8 +56,8 @@ def read_xls_into_dataframe(fname, sheet, keyname, multiline=False):
                 dataframe[key].append("NEWLINE")                
     return dataframe, headers
 
-def read_xls_into_dataset(fname, sheet, keyname):
-    dictionary = read_xls_into_dictionary(fname,sheet)
+def read_xls_into_dataset(fname, sheet, keyname, xf = True):
+    dictionary = read_xls_into_dictionary(fname,sheet, xf)
     headers = dictionary.keys()
     lids = dictionary[keyname]
     dataset = {}
@@ -62,8 +70,8 @@ def read_xls_into_dataset(fname, sheet, keyname):
             dataset[lid][key].append(col[num])
     return dataset
 
-def read_xls_into_dataset1(fname, sheet, keyname):
-    dataset = read_xls_into_dataset(fname,sheet,keyname)
+def read_xls_into_dataset1(fname, sheet, keyname, xf = True):
+    dataset = read_xls_into_dataset(fname,sheet,keyname, xf)
     for key1, idict in dataset.items():
         for key2 in idict.keys():
             dataset[key1][key2] = dataset[key1][key2][0]
--- write.py
+++ write.py
@@ -1,15 +1,37 @@
 import xlwt
 
-def write_xls_from_matrix(fname, sheet_name, matrix):
-    wb = xlwt.Workbook()
+
+def fill_sheet_from_matrix(ws, matrix, justify = True, styles = None):
+    transpose = zip(*matrix)
+    for c,col in enumerate(transpose):
+        maxlen = 0
+        for r,cell in enumerate(col):            
+            value = cell
+            if isinstance(cell,(tuple,list)):
+                if cell[0]==0:
+                    value = cell[1]
+                    style = cell[2]
+                elif cell[0]==1:
+                    value = cell[1].value
+                    style = cell[1].xf_index
+                ws.write(r, c, value, styles[style])
+            else:
+                ws.write(r, c, value)
+            maxlen = max(maxlen,len(unicode(value)))
+        ws.col(c).width = 256 * (maxlen + 2) 
+    return ws
+
+def add_sheet_from_matrix(wb, sheet_name, matrix, justify = True, styles = None):
     ws = wb.add_sheet(sheet_name)    
-    for r,row in enumerate(matrix):
-        for c,cell in enumerate(row):
-            ws.write(r, c, cell)
+    ws = fill_sheet_from_matrix(ws,matrix, justify, styles)
+    return ws
+
+def write_xls_from_matrix(fname, sheet_name, matrix, justify=True, styles = None):
+    wb = xlwt.Workbook()
+    ws = add_sheet_from_matrix(wb,sheet_name,matrix, justify, styles)
     wb.save(fname)
 
-
-def write_xls_from_dictionary(fname, sheet_name, dictionary, headers = None):
+def write_xls_from_dictionary(fname, sheet_name, dictionary, headers = None, justify=True, styles = None):
     mheaders = sorted(dictionary.keys())
     if headers is None:
         headers = mheaders
@@ -23,14 +45,14 @@ def write_xls_from_dictionary(fname, sheet_name, dictionary, headers = None):
         column = dictionary[key]
         for r,cell in enumerate(column):
             matrix[r+1].append(cell)
-    write_xls_from_matrix(fname, sheet_name, matrix)
+    write_xls_from_matrix(fname, sheet_name, matrix, justify, styles)
     return matrix
 
     
 def write_xls_from_dataframe(fname, sheet, keyname, headers, multiline=0):
     pass
 
-def write_xls_from_dataset(fname, sheet, dataset, headers = None):    
+def write_xls_from_dataset(fname, sheet, dataset, headers = None, justify=True, styles = None):    
     """Dataset Keys are ID"""
     mheaders = sorted(dataset.values()[0].keys())
     if headers is None:
@@ -48,9 +70,9 @@ def write_xls_from_dataset(fname, sheet, dataset, headers = None):
                 data[column] = []
             for value in values:
                 data[column].append(value)
-    write_xls_from_dictionary(fname,sheet,data,headers)
+    write_xls_from_dictionary(fname,sheet,data,headers, justify, styles)
 
-def write_xls_from_dataset1(fname, sheet, dataset, headers = None ):
+def write_xls_from_dataset1(fname, sheet, dataset, headers = None, justify=True, styles = None):
     dataset1 = {}
     for key, dictz in dataset.items():
         if key not in dataset1:
@@ -59,5 +81,5 @@ def write_xls_from_dataset1(fname, sheet, dataset, headers = None ):
             if column not in dataset1[key]:
                 dataset1[key][column] = {}
             dataset1[key][column] = [ value, ]
-    write_xls_from_dataset(fname, sheet, dataset1, headers )
+    write_xls_from_dataset(fname, sheet, dataset1, headers, justify, styles )
 
